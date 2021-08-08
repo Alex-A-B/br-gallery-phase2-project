5c71abbff009c5048c442e9bd2b9fd2833db767a
Alex-A-B <alexjamesbelton@gmail.com>
2021-08-08 11:33:27 +0100

    Error pages generated for out of scope images

--- src/components/ArtworkDetails.js
+++ src/components/ArtworkDetails.js
@@ -2,13 +2,19 @@ import { useState } from "react"
 
 function ArtworkDetails( { painting }) {
     const [extraDetails, setExtraDetails] = useState(false)
-    const {img_src, painting_title, season, episode} = painting
+    const {img_src, painting_title, season, episode, /*colors*/} = painting
 
     function handleExtraDetails(){
         setExtraDetails(!extraDetails)
     }
 
- 
+    // console.log(colors)
+
+    // const listColors = colors.map( (color) => {return <li> {color} </li> })
+   
+    if (Object.keys(painting).length < 1 ) {
+        return <h1 className="home">Loading...</h1>
+    }
 
     return (
         <div className="artwork home" onClick={handleExtraDetails}>
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,9 @@
+function ErrorPage( { error }) {
+    return (
+        <div className="home">
+            <h2>{error}</h2>
+        </div>
+    )
+}
+
+export default ErrorPage
